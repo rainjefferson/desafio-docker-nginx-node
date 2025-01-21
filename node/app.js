c8934a7dbf92df139edc88762f815ec7896eef04
rainjefferson <jeffer@hotmail.com.br>
2025-01-20 22:16:48 -0300

    Corrigido itens abaixo: 1 - O container do nginx também deve estar presente no projeto, sem ele nesse determinado contexto não funcionar. Adicione novamente. 2 - Reavalie a estrutura do seu projeto, porque atualmente está tentando utilizar algumas coisas como um Dockerfile por exemplo sem possuir um arquivo dockerfile ou o caminho não foi definido corretamente

--- node/app.js
+++ node/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const config = {
     host: process.env.MYSQL_HOST || 'mysql',
     user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || 'appdb',
 };
 
